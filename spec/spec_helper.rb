181787919085f80655972b7cc807f7a41f28b1f0
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-12-08 00:49:11 +0100

    update the spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
+ENV["RAILS_ENV"] ||= 'test'
+
 require 'rack/test'
 require 'payloads'
+
+require 'travis/listener'
+
+Travis.logger = Logger.new(StringIO.new)
+
+Travis::Listener.setup
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods
