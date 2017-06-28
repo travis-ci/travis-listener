13c1f3f084292e2bb9add30ebbc7397bb2f7e306
Igor Wiedler <igor@travis-ci.org>
2017-06-28 15:39:02 +0200

    use LISTENER_REDIS_URL env var

--- lib/travis/listener.rb
+++ lib/travis/listener.rb
@@ -15,8 +15,7 @@ module Travis
 
   module Listener
     class Config < Travis::Config
-      define  redis:          { url: 'redis://localhost:6379', namespace: 'sidekiq', network_timeout: 5 },
-              listener_redis: { url: 'redis://localhost:6379', namespace: 'sidekiq', network_timeout: 5 },
+      define  redis:          { url: ENV['LISTENER_REDIS_URL'] || 'redis://localhost:6379', namespace: 'sidekiq', network_timeout: 5 },
               gator:          { queue: ENV['SIDEKIQ_GATEKEEPER_QUEUE'] || 'build_requests' },
               sentry:         { },
               metrics:        { reporter: 'librato' }
@@ -25,11 +24,7 @@ module Travis
     class << self
       def setup
         ::Sidekiq.configure_client do |config|
-          if ENV['LISTENER_REDIS_ENABLED'] == 'true'
-            config.redis = Travis.config.listener_redis.to_h
-          else
-            config.redis = Travis.config.redis.to_h
-          end
+          config.redis = Travis.config.redis.to_h
         end
 
         if Travis.config.sentry.dsn
