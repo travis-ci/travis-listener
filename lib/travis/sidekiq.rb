eaed25e9d5f80952e116f7e41060b6803752ae69
GbArc <gabriel.arczynski@devtactics.net>
2023-08-25 12:34:38 +0200

    sidekiq push

--- lib/travis/sidekiq.rb
+++ lib/travis/sidekiq.rb
@@ -15,7 +15,7 @@ module Travis
         client.push(
           'queue' => queue,
           'class' => 'Travis::Gatekeeper::Worker',
-          'args' => args
+          'args' => args.map! { |arg| arg.to_json }
         )
       end
     end
